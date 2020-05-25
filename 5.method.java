4b256baaaeee55ccaf7e43ae6f6f1496f045e5a8
kiwort <48189172+kiwort@users.noreply.github.com>
2020-05-25 10:38:28 +0530

    Update 5.method.java

--- 5.method.java
+++ 5.method.java
@@ -1,10 +1,16 @@
 public class Mango {	
 	public static void  main(String[] args) {
 		sayHello();
+		int sum= add(100,100,100);
+		System.out.println(sum);
 		
 	}
 	public static void sayHello() {
 		System.out.println("I AM  KISHAN HERE AND SAYING YOU HELLO");
+	}
+	public static int add(int a ,int b, int c) {
+	int d = (a+b+c);
+	return d;
 	}
 
 }
