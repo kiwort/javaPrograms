6ffd74eaaba2b9704aee191a833528fee5429868
kiwort <48189172+kiwort@users.noreply.github.com>
2020-05-25 10:24:45 +0530

    Create 5.method.java

---
+++ 5.method.java
@@ -0,0 +1,10 @@
+public class Mango {	
+	public static void  main(String[] args) {
+		sayHello();
+		
+	}
+	public static void sayHello() {
+		System.out.println("I AM  KISHAN HERE AND SAYING YOU HELLO");
+	}
+
+}
