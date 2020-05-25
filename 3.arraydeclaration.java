c764c2b5bd3c4b19cb7a9e9c6192a4a91bbee261
kiwort <48189172+kiwort@users.noreply.github.com>
2020-05-25 10:04:59 +0530

    Create 3.arraydeclaration.java

---
+++ 3.arraydeclaration.java
@@ -0,0 +1,16 @@
+public class Mango {	
+	public static void  main(String[] args) {
+		//declaring array
+		int[] array = {1,2,3};
+		//int[] array1 = new int[5];
+		//int[] array2 = new int[] {1,2,3};
+		
+		int i=0;
+		while(i<3) {
+			System.out.println(array[i]);
+			i++;
+		}
+		
+	}
+
+}
